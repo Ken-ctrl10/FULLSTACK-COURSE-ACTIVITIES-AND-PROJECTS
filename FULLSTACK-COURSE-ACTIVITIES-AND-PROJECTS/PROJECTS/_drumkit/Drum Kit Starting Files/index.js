f6a45e568a19931e01e103658b2194968150c5b4
Ken <PrinceKenneth.Castillo@cognizant.com>
2026-05-12 13:55:57 +0800

    Finished the drumkit mini-activity, and modify some files on the dice directory

---
+++ FULLSTACK-COURSE-ACTIVITIES-AND-PROJECTS/PROJECTS/_drumkit/Drum Kit Starting Files/index.js
@@ -0,0 +1,44 @@
+function playSound(key) {
+  switch(key) {
+    case 'w':
+      new Audio('./sounds/tom-1.mp3').play();
+      break;
+
+    case 'a':
+      new Audio('./sounds/tom-2.mp3').play();
+      break;
+
+    case 's':
+      new Audio('./sounds/tom-3.mp3').play();
+      break;
+
+    case 'd':
+      new Audio('./sounds/tom-4.mp3').play();
+      break;
+
+    case 'j':
+      new Audio('./sounds/snare.mp3').play();
+      break;
+
+    case 'k':
+      new Audio('./sounds/kick-bass.mp3').play();
+      break;
+
+    case 'l':
+      new Audio('./sounds/crash.mp3').play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+document.querySelectorAll('.drum').forEach(function(button){
+  button.addEventListener('click', () => {
+    playSound(button.innerHTML);
+  })
+})
+
+document.addEventListener('keydown', (event) => {
+  playSound(event.key);
+})
